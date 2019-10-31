a40cec0dfe600165b26e79d3b810a542044ee4ca
Jacob Kosberg <jacob.a.kosberg@jpl.nasa.gov>
2019-10-30 17:08:53 -0700

    Update detailed_timelapse.py

--- timelapse_creator/detailed_timelapse.py
+++ timelapse_creator/detailed_timelapse.py
@@ -20,11 +20,11 @@ class Temps(object):
     def __init__(self, infile):
         self.df = pd.read_pickle(infile)
 
-        #print(self.df.shape)
-        #self.df = self.df.loc[~self.df.index.duplicated(keep='first')]
-        #print(self.df.shape)
+        print(self.df.shape)
+        self.df = self.df.loc[~self.df.index.duplicated(keep='first')]
+        print(self.df.shape)
 
-    def on(self, date):
+    def on(self, date, temp_column):
         d = datetime.strptime(date, "%Y-%m-%d_%H-%M-%S")
         #true_index = self.df["timestamp"].index.get_loc(d, method='nearest')
         #select_indices = list(np.where(abs(df["timestamp"] - d))[0])
@@ -32,19 +32,33 @@ class Temps(object):
 
         #print(self.df.iloc[true_index])
         #print(datetime.strptime(date, "%Y-%m-%d_%H-%M-%S"))
-        return self.df.iloc[true_index]["avg_t"]
+        return self.df.iloc[true_index][temp_column]
+
+    def addTemps(self, img, datestr, cols, startx, starty):
+        dy = 70
+        i = 0
+        for col in cols:
+            text = cols[col] + ": "
+            temp = format(self.on(datestr, temp_column=col), '.1f') + " K"
+            # align numbers by splitting TC names and values
+            img = addTemp(img, text, startx, starty+dy*i)
+            img = addTemp(img, temp, startx+500, starty+dy*i)
+            i += 1
+        return img
 
 def imload(path):
     image = cv2.imread(path)
     #return image
-    return cv2.resize(image,None,fx=0.75, fy=0.75, interpolation = cv2.INTER_LINEAR)
+    return cv2.resize(image,None,fx=0.50, fy=0.50, interpolation = cv2.INTER_LINEAR)
 
 def addTemp(img, text, x, y):
-    cv2.putText(img,text, (x,y), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=4, color=(255,255,255), thickness=4)
+    cv2.putText(img,text, (x,y), fontFace=cv2.FONT_HERSHEY_TRIPLEX, fontScale=1.7, color=(255,255,255), thickness=1)
     return img
 
+
+
 def addTime(img, text):
-    cv2.putText(img,text, (25, 35), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1, color=(150,150,150))
+    cv2.putText(img,text, (25, 35), fontFace=cv2.FONT_HERSHEY_DUPLEX, fontScale=1.5, color=(150,150,150))
     return img
 
 def main():
@@ -67,16 +81,17 @@ def main():
 
     writer = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*"MJPG"), 30, (width, height))
 
-    temps = Temps("/Users/manifolds/code/data-tools/data_image.pkl")
+    temps = Temps(r"C:\Users\kosberg\code\data-tools\data_image.pkl")
 
     # write image (k*j + s) for all j = i-s = 0,...,len(images)-s
     for i in tqdm(range(args.skip, len(images))):
-        datestr = images[i].split("/")[-1].split(".")[0]
+        datestr = images[i].split("\\")[-1].split(".")[0]
         date, time = datestr.split("_")
         time = ":".join(time.split("-"))
 
         if (i-args.skip) % args.harmonic != 0:
             continue
+        
         try:
             img = imload(images[i])
             #aise cv2.error("test error")
@@ -88,8 +103,8 @@ def main():
             print("Invalid shape: " + str(img.shape))
             continue
 
-        img = addTemp(img, format(temps.on(datestr), '.1f') + " K", x=int(width/7), y=int(height/3))
         img = addTime(img, date + "  " + time)
+        img = temps.addTemps(img, datestr, cols={"TC_20":"Peak", "TC_18":"Under Peak", "TC_15":"Trough"}, startx=int(width/9), starty=int(height/5))
 
         writer.write(img)
     writer.release()
